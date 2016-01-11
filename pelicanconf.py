6343dda587ed7e41ab0ddf3006aaa35f4e477219
Swapnil Kulkarni (coolsvap) <me@coolsvap.net>
2016-01-11 16:16:35 +0530

    Initial Setup of Pelican Site
    Signed-off-by: Swapnil Kulkarni <me@coolsvap.net>

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Swapnil Kulkarni (coolsvap)'
+SITENAME = u'Swapnil Kulkarni Blog'
+SITEURL = 'coolsvap.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/coolsvap'),
+          ('Facebook', 'https://www.facebook.com/coolsvap'),
+          ('LinkedIn', 'https://www.in.linkedin.com/coolsvap'),
+          ('GitHub', 'https://www.github.com/coolsvap'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
